45359da110a191664f3c50b34db55bb344d58cf4
sidarth16 <ssidarth1999@gmail.com>
2022-11-20 10:12:37 +0530

    deploy ERC20 Tokens

--- scripts/deployTokens.js
+++ scripts/deployTokens.js
@@ -12,18 +12,41 @@ async function main() {
   console.log("deployer : ", deployer.address);
 
   const wethFactory = await ethers.getContractFactory("WETH");
+  const wbtcFactory = await ethers.getContractFactory("WBTC");
+  const wbnbFactory = await ethers.getContractFactory("WBNB");
+
   const weth = await wethFactory.deploy();
-  console.log("Waiting for tx Confirmation");
   await weth.deployTransaction.wait(6);
-
   console.log("Deployed Weth at : ",weth.address);
-
   console.log("Verifying WETH : ")
   await hre.run("verify:verify", {
-    address: "0x066e2511fe43332A729b5d977dfCCe4F0C493FcD",
-    contract: "contracts/WETH.sol:WETH",
+    address: weth.address,
+    constructorArguments: [],
+    contract: "contracts/WETH.sol:WETH"
+  });
+
+
+  const wbtc = await wbtcFactory.deploy();
+  await wbtc.deployTransaction.wait(6);
+  console.log("Deployed WBTC at : ",wbtc.address);
+  console.log("Verifying WBTC : ")
+  await hre.run("verify:verify", {
+    address: wbtc.address,
+    contract: "contracts/WBTC.sol:WBTC",
     constructorArguments: [],
   });
+
+
+  const wbnb = await wbnbFactory.deploy();
+  await wbnb.deployTransaction.wait(6);
+  console.log("Deployed WBNB at : ",wbnb.address);
+  console.log("Verifying NFT : ")
+  await hre.run("verify:verify", {
+    address: wbnb.address,
+    contract: "contracts/WBNB.sol:WBNB",
+    constructorArguments: [],
+  });
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
